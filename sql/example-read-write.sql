6cccfc1a62d550aa3f296771bba7fc02962e1b50
Rupali L <luckrupali22@gmail.com>
2025-07-11 16:27:19 +0530

    Updated example-read-write.sql

--- sql/example-read-write.sql
+++ sql/example-read-write.sql
@@ -1,5 +1,5 @@
--- Write query (goes to RDS/Aurora writer endpoint)
-INSERT INTO orders (id, customer, amount) VALUES (101, 'Rupali', 1500);
+-- Write query goes to RDS (primary DB)
+INSERT INTO orders (id, customer_name, amount) VALUES (101, 'Rupali', 2500);
 
--- Read query (goes to Aurora reader endpoint)
-SELECT * FROM orders WHERE customer = 'Rupali';
+-- Read query goes to Aurora Reader endpoint
+SELECT * FROM orders WHERE customer_name = 'Rupali';
